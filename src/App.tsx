8c49d79547731e5d58cd47664b0eacb12f9ac384
Julio Zittei <julio.zittei96@gmail.com>
2021-11-17 18:29:07 -0300

    Add miragejs and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return {
+        id: 1,
+        title: 'Desenvolvimento',
+        amount: 1000,
+        type: 'income',
+        category: 'Trabalho',
+        createdAt: new Date(),
+      };
+    });
+  },
+});
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
