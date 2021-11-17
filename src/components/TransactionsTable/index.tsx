d8b5db2093b4abe1d4ffdb500fef2b6b3e3aa2bd
Julio Zittei <julio.zittei96@gmail.com>
2021-11-17 18:27:52 -0300

    Add Component TransactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import { Container } from './styles';
+
+export function TransactionsTable() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de WebSite</td>
+            <td className="income">R$ 1000,00</td>
+            <td>Trabalho</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="outcome">-R$ 500,00</td>
+            <td>Casa</td>
+            <td>20/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
