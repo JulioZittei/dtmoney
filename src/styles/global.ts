9d91fd1aff77c9b04a7332ab2b5d87c448621f77
Julio Zittei <julio.zittei96@gmail.com>
2021-11-17 18:28:36 -0300

    Add new color

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
 :root {
   --red: #e52e4d;
   --blue: #5429cc;
+  --green: #33cc95;
 
   --blue-light: #6933ff;
 
@@ -33,7 +34,7 @@ html {
  
 
 body {
-  background: var(---background);
+  background: var(--background);
   -webkit-font-smoothing: antialiased;
 }
 
