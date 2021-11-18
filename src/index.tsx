d9bbeaec2ffae8ba285c578f2d74f2645131ee56
Julio Zittei <julio.zittei96@gmail.com>
2021-11-18 02:35:03 -0300

    Update and move miragejs configs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer, Model } from 'miragejs';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de Website',
+          category: 'Trabalho',
+          type: 'income',
+          amount: 1000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          category: 'Casa',
+          type: 'outcome',
+          amount: 500,
+          createdAt: new Date('2021-02-14 11:00:00'),
+        },
+      ],
+    });
+  },
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    });
+    this.post('transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', {
+        ...data,
+        createdAt: new Date(),
+      });
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
